073dac7d8527cb4241bd39b55ff056fbc5753003
prtzl <matej.blagsic@protonmail.com>
2025-09-12 22:40:45 +0200

    Update base for systems

--- profiles/system/base.nix
+++ profiles/system/base.nix
@@ -1,9 +1,10 @@
 {
+  config,
   inputs,
   lib,
   machineInfo,
-  pkgs,
   pillow,
+  pkgs,
   ...
 }:
 
@@ -21,9 +22,11 @@
     ];
 
   # Some stuff that can change based on system, so use mkDefault
-  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
-  boot.loader.efi.canTouchEfiVariables = lib.mkDefault true;
+  boot = {
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+    loader.systemd-boot.enable = lib.mkDefault true;
+    loader.efi.canTouchEfiVariables = lib.mkDefault true;
+  };
 
   nix = {
     monitored.notify = false;
@@ -61,52 +64,73 @@
     interface: "--interface=${interface}"
   ) machineInfo.interfaces;
 
-  networking.useDHCP = true;
-  networking.hostName = machineInfo.hostname;
-  services.xserver.xkb.layout = "us";
+  networking = {
+    enableIPv6 = false;
+    firewall.enable = true;
+    hostName = machineInfo.hostname;
+    networkmanager.enable = true;
+    useDHCP = true;
+  };
   i18n.defaultLocale = "en_GB.UTF-8";
-
-  programs.firefox = {
-    enable = (pillow.edition == "workstation");
-    package = pkgs.firefox;
+  services = {
+    xserver.xkb.layout = "us";
+    fwupd.enable = true;
   };
 
-  programs.usbtop.enable = true;
+  programs = {
+    firefox = {
+      enable = (pillow.edition == "workstation");
+      package = pkgs.firefox;
+    };
+    usbtop.enable = true;
+    zsh.enable = true;
+  };
 
-  environment.systemPackages =
-    with pkgs;
-    [
-      bat # replacement for cat
-      btop # system info graphs, usage, etc. Modern top
-      eza # replacement for exa, replacement for ls
-      fastfetch # replacement for neofetch :'(
-      fd # modern find
-      fx # json  viewer
-      jq # json processor
-      parted # partitions
-      ripgrep # modern grep
-      smartmontools # disk checks
-      xh
-    ]
-    ++ lib.optionals (pillow.edition == "workstation") [
-      # file explorer
-      xfce.thunar
-      xfce.thunar-archive-plugin
-      xfce.tumbler
+  users.defaultUserShell = config.programs.zsh.package;
 
-      # calculator
-      qalculate-gtk
-
-      # media
-      vlc # video player
-      gthumb # image viewer
-
-      # system utils
-      usbutils # info on usb devices
-      hwinfo # self explanatory
-      pciutils # info on pci devices
-      monitorets # GUI for temperature sensors
+  environment = {
+    shells = with pkgs; [
+      bashInteractive
+      zsh
     ];
+    variables = {
+      EDITOR = "vim";
+    };
+    systemPackages =
+      with pkgs;
+      [
+        bat # replacement for cat
+        btop # system info graphs, usage, etc. Modern top
+        eza # replacement for exa, replacement for ls
+        fastfetch # replacement for neofetch :'(
+        fd # modern find
+        fx # json  viewer
+        jq # json processor
+        parted # partitions
+        ripgrep # modern grep
+        smartmontools # disk checks
+        xh
+      ]
+      ++ lib.optionals (pillow.edition == "workstation") [
+        # file explorer
+        xfce.thunar
+        xfce.thunar-archive-plugin
+        xfce.tumbler
+
+        # calculator
+        qalculate-gtk
+
+        # media
+        vlc # video player
+        gthumb # image viewer
+
+        # system utils
+        usbutils # info on usb devices
+        hwinfo # self explanatory
+        pciutils # info on pci devices
+        monitorets # GUI for temperature sensors
+      ];
+  };
 
   xdg.mime.inverted.defaultApplications."gthumb.desktop" =
     lib.optionals (pillow.edition == "workstation")
