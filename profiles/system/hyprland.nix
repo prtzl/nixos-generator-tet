f0714e0f516186836b14a349eeb21b12f18b867c
prtzl <matej.blagsic@protonmail.com>
2025-09-12 19:16:29 +0200

    Add hyprland

---
+++ profiles/system/hyprland.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  programs.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+    xwayland.enable = true;
+  };
+
+  # Light weight and nice
+  services.displayManager.ly = {
+    enable = true;
+    settings = {
+      save = true; # save current session as default - handy
+      load = true; # save current login username
+    };
+  };
+
+  # needed by xfce apps to save config - yikes, well, at least not gnome
+  programs.xfconf.enable = true;
+
+  # Fixes electron apps in wayland, so I've read.
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+}
