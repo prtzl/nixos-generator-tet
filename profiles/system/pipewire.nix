f789903e083814dcacb8b9b8ad99246c12a05d6f
prtzl <matej.blagsic@protonmail.com>
2025-09-12 19:31:25 +0200

    Add pipewire

---
+++ profiles/system/pipewire.nix
@@ -0,0 +1,40 @@
+{
+  ...
+}:
+
+{
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    wireplumber = {
+      enable = true;
+    };
+    extraConfig.pipewire.samplerate = {
+      "context.properties" = {
+        "log.level" = 2;
+
+        "default.clock.rate" = 44100;
+        "default.clock.allowed-rates" = [
+          44100
+          48000
+          88200
+          96000
+        ];
+
+        "default.clock.quantum" = 512; # ~10.64mn latency
+        "default.clock.min" = 32;
+        "default.clock.max-quantum" = 4096;
+        "default.clock.quantum-limit" = 4096;
+        "clock.power-of-two-quantum" = true; # more efficient
+      };
+    };
+  };
+
+  services.pulseaudio.enable = false;
+}
