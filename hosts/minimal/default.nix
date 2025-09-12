43a907758893ea7355d1b2f1e64697e50ec9f0d0
prtzl <matej.blagsic@protonmail.com>
2025-09-12 16:16:59 +0000

    Huh, got a working vm with this. I can't believe it.

--- hosts/minimal/default.nix
+++ hosts/minimal/default.nix
@@ -28,9 +28,6 @@ my.mkSystem {
         networking.hostName = "minimal";
         networking.useDHCP = true;
 
-        time.timeZone = "UTC";
-
-        services.openssh.enable = true;
 
         environment.systemPackages = with pkgs; [
           vim
@@ -38,17 +35,36 @@ my.mkSystem {
           curl
         ];
 
-        # ❗ Required: root filesystem definition
-        fileSystems."/" = {
-          device = "/dev/sda2";
-          fsType = "ext4"; # or btrfs/xfs/etc.
-        };
+        #
 
-        # Optional: boot partition if using EFI
-        fileSystems."/boot" = {
-          device = "/dev/sda1";
-          fsType = "vfat";
-        };
+  boot.initrd.availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/d345cdce-53f7-4474-b546-a7141106fa1b";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/29DC-059E";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/3205bd4b-d777-40d8-a1f6-5c13ed6ffdf5"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
 
         # ❗ Required: root filesystem definition
         # fileSystems."/" = {
