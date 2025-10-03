22f6b35239a9ba8f83213ffcff614a536f678143
prtzl <matej.blagsic@protonmail.com>
2025-10-03 14:13:17 +0000

    Mount storage with disko, ok, seems to work.

--- hosts/poli/disko.nix
+++ hosts/poli/disko.nix
@@ -36,23 +36,45 @@
       };
     };
 
-    storageSSD = {
-      device = "/dev/disk/by-id/nvme-Lexar_SSD_NM790_4TB_QER497R000155P220";
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          root = {
-            label = "storage";
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "xfs";
-              mountpoint = "/storage";
-            };
-          };
-        };
-      };
+    # storage = {
+    #   device = "/dev/disk/by-id/nvme-Lexar_SSD_NM790_4TB_QER497R000155P220";
+    #   type = "disk";
+    #   content = {
+    #     type = "gpt"; # Optional if already formatted
+    #     partitions = {
+    #       data = {
+    #         label = "storage";
+    #         size = "100%";
+    #         content = {
+    #           type = "filesystem";
+    #           format = "xfs";
+    #           mountpoint = "/storage";
+    #           mountOptions = [
+    #             "defaults"
+    #             "user"
+    #             "rw"
+    #             "exec"
+    #           ];
+    #         };
+    #       };
+    #     };
+    #   };
+    # };
+  };
+
+  # Aparrently since it's already formatted use it like this and it won't ever attempt to format it
+  # even on a nixos-install or disko install. It will skip it
+  disko.devices.nodev = {
+    storage = {
+      device = "/dev/disk/by-label/storage"; # or by-id
+      fsType = "xfs";
+      mountpoint = "/storage";
+      mountOptions = [
+        "defaults"
+        "user"
+        "rw"
+        "exec"
+      ];
     };
   };
 }
