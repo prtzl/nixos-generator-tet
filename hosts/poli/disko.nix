05d28cc893f03bbc0c687c45c36225067401ab05
prtzl <matej.blagsic@protonmail.com>
2025-10-03 12:20:31 +0200

    Update disko for poli

--- hosts/poli/disko.nix
+++ hosts/poli/disko.nix
@@ -1,40 +1,54 @@
 {
-  disko.devices = {
-    disk = {
-      my-disk = {
-        device = "/dev/disk/by-id/nvme-WD_BLACK_SN8100_2000GB_25221P801548";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              label = "EFI";
-              type = "EF00";
-              size = "1G";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
-              };
+  disko.devices.disk = {
+    main = {
+      device = "/dev/disk/by-id/nvme-WD_BLACK_SN8100_2000GB_25221P801548";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            label = "boot";
+            size = "1M";
+            type = "EF02";
+            priority = 0;
+          };
+          ESP = {
+            label = "EFI";
+            type = "EF00";
+            size = "1G";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
             };
-            root = {
-              label = "nixos";
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "xfs";
-                mountpoint = "/";
-              };
+          };
+          root = {
+            label = "nixos";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/";
             };
-            fastStorage = {
-              label = "storage";
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "xfs";
-                mountpoint = "/storage";
-              };
+          };
+        };
+      };
+    };
+
+    storageSSD = {
+      device = "/dev/disk/by-id/nvme-Lexar_SSD_NM790_4TB_QER497R000155P220";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          root = {
+            label = "storage";
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "xfs";
+              mountpoint = "/storage";
             };
           };
         };
