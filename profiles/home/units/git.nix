867a4f9257c8e74cade7574e917a71cd2c61e99b
prtzl <matej.blagsic@protonmail.com>
2025-10-04 20:00:10 +0200

    Tell git to piss off, I'm very smart

--- profiles/home/units/git.nix
+++ profiles/home/units/git.nix
@@ -8,8 +8,12 @@
     enable = true;
     difftastic.enable = true;
     extraConfig = {
-      log.date = lib.mkDefault "iso";
       core.init.defaultBranch = lib.mkDefault "master";
+      log.date = lib.mkDefault "iso";
+      # idea is that dir is owned by root but
+      # maintainers are in group that has access
+      # Git doesn't like this, so I'm placing this here
+      safe.directory = "/etc/nixos";
     };
   };
 }
