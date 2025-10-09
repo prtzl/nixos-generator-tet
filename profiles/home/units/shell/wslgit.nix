6bf32d87d6ea1b806459278f4b002aefb2baf810
prtzl <matej.blagsic-ext@hexagon.com>
2025-10-09 10:09:50 +0200

    Fixup wslgit, wrong swithc, pritty up

--- profiles/home/units/shell/wslgit.nix
+++ profiles/home/units/shell/wslgit.nix
@@ -8,17 +8,25 @@ let
     name = "wslgit";
     runtimeInputs = with pkgs; [ git ];
     text = ''
-      # checks to see if we are in a windows or linux dir
-      function isWinDir {
-        case $PWD/ in
-          /mnt/*) return "$(true)";;
-          *) return "$(false)";;
-        esac
+      # Detect if current directory is on a Windows-mounted drive (/mnt/c, /mnt/d, etc.)
+      is_win_dir() {
+        [[ "$PWD" =~ ^/mnt/[a-zA-Z]/ ]]
       }
-      # wrap the git command to either run windows git or linux
-      if isWinDir
-      then
-        git.exe "$@"
+
+      # Find git.exe if available (returns 0 if found)
+      find_win_git() {
+        which git.exe >/dev/null 2>&1
+      }
+
+      if is_win_dir; then
+        if find_win_git; then
+          # Use Windows git.exe
+          git_exe_path="$(which git.exe)"
+          "$git_exe_path" "$@"
+        else
+          echo "⚠️  Windows git.exe not found!"
+          return 1
+        fi
       else
         git "$@"
       fi
