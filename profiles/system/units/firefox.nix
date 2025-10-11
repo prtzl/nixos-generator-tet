0e1eb1697b8eec88e65668bb5f61609c7f526a9e
prtzl <matej.blagsic@protonmail.com>
2025-10-11 22:55:31 +0200

    Add all my firefox extensions

--- profiles/system/units/firefox.nix
+++ profiles/system/units/firefox.nix
@@ -51,6 +51,30 @@ in
           install_url = "https://addons.mozilla.org/firefox/downloads/file/3940751/tabliss-2.6.0.xpi";
           installation_mode = "force_installed";
         };
+        "firefox-enpass@enpass.io" = {
+          install_url = "https://dl.enpass.io/stable/extensions/firefox/versions/v6.11.7.2/enpass_password_manager-6.11.7.2.xpi";
+          installation_mode = "force_installed";
+        };
+        "idcac-pub@guus.ninja" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "user-agent-switcher@ninetailed.ninja" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/uaswitcher/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{b271a84f-6f7c-4784-9b00-3c1b4faa2dce}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-comment-search/latest.xpi";
+          installation_mode = "force_installed";
+        };
+        "{174b2d58-b983-4501-ab4b-07e71203cb43}" = {
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/dark-mode-webextension/latest.xpi";
+          installation_mode = "force_installed";
+        };
       };
     };
   };
