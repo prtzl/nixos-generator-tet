796f300124fd0c070fe446ddefacab65c64a2551
prtzl <matej.blagsic@protonmail.com>
2025-10-18 13:11:54 +0200

    Leave firefox configuration alone for now until it's final - now some UI stuff is not how I would like it. Don't break my shit.

--- profiles/home/units/firefox.nix
+++ profiles/home/units/firefox.nix
@@ -7,48 +7,48 @@
   programs.firefox = {
     enable = pillow.hasGUI;
 
-    profiles = {
-      default = {
-        id = 0;
-        name = "default";
-        isDefault = true;
-
-        settings = {
-          # "browser.startup.homepage" = "https://duckduckgo.com";
-          "browser.search.defaultenginename" = "ddg";
-          "browser.search.order.1" = "DuckDuckGo";
-
-          "signon.rememberSignons" = false;
-          "widget.use-xdg-desktop-portal.file-picker" = 1;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.compactmode.show" = true;
-          "browser.cache.disk.enable" = false; # Be kind to hard drive
-          "browser.toolbars.bookmarks.visibility" = "never";
-
-          # "mousewheel.default.delta_multiplier_x" = 20;
-          # "mousewheel.default.delta_multiplier_y" = 20;
-          # "mousewheel.default.delta_multiplier_z" = 20;
-
-          # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
-          # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
-          # then have Firefox open on some other workspace.
-          "widget.disable-workspace-management" = true;
-
-          "sidebar.verticalTabs" = true;
-          "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
-          "sidebar.main.tools" = "history,bookmarks";
-          "sidebar.visibility" = "hide";
-        };
-
-        search = {
-          force = true;
-          default = "ddg";
-          order = [
-            "ddg"
-            "google"
-          ];
-        };
-      };
-    };
+    # profiles = {
+    #   default = {
+    #     id = 0;
+    #     name = "default";
+    #     isDefault = true;
+    #
+    #     settings = {
+    #       # "browser.startup.homepage" = "https://duckduckgo.com";
+    #       "browser.search.defaultenginename" = "ddg";
+    #       "browser.search.order.1" = "DuckDuckGo";
+    #
+    #       "signon.rememberSignons" = false;
+    #       "widget.use-xdg-desktop-portal.file-picker" = 1;
+    #       "browser.aboutConfig.showWarning" = false;
+    #       "browser.compactmode.show" = true;
+    #       "browser.cache.disk.enable" = false; # Be kind to hard drive
+    #       "browser.toolbars.bookmarks.visibility" = "never";
+    #
+    #       # "mousewheel.default.delta_multiplier_x" = 20;
+    #       # "mousewheel.default.delta_multiplier_y" = 20;
+    #       # "mousewheel.default.delta_multiplier_z" = 20;
+    #
+    #       # Firefox 75+ remembers the last workspace it was opened on as part of its session management.
+    #       # This is annoying, because I can have a blank workspace, click Firefox from the launcher, and
+    #       # then have Firefox open on some other workspace.
+    #       "widget.disable-workspace-management" = true;
+    #
+    #       "sidebar.verticalTabs" = true;
+    #       "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+    #       "sidebar.main.tools" = "history,bookmarks";
+    #       "sidebar.visibility" = "hide";
+    #     };
+    #
+    #     search = {
+    #       force = true;
+    #       default = "ddg";
+    #       order = [
+    #         "ddg"
+    #         "google"
+    #       ];
+    #     };
+    #   };
+    # };
   };
 }
--- profiles/system/units/firefox.nix
+++ profiles/system/units/firefox.nix
@@ -25,57 +25,57 @@ in
   programs.firefox = {
     enable = pillow.hasGUI;
 
-    policies = {
-      DisableTelemetry = true;
-      DisableFirefoxStudies = true;
-      DontCheckDefaultBrowser = true;
-      DisablePocket = true;
-      SearchBar = "unified";
-
-      Preferences = {
-        # Privacy settings
-        "extensions.pocket.enabled" = lock-false;
-        "browser.newtabpage.pinned" = lock-empty-string;
-        "browser.topsites.contile.enabled" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
-      };
-
-      ExtensionSettings = {
-        "uBlock0@raymondhill.net" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "extension@tabliss.io" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/3940751/tabliss-2.6.0.xpi";
-          installation_mode = "force_installed";
-        };
-        "firefox-enpass@enpass.io" = {
-          install_url = "https://dl.enpass.io/stable/extensions/firefox/versions/v6.11.7.2/enpass_password_manager-6.11.7.2.xpi";
-          installation_mode = "force_installed";
-        };
-        "idcac-pub@guus.ninja" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "user-agent-switcher@ninetailed.ninja" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/uaswitcher/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{b271a84f-6f7c-4784-9b00-3c1b4faa2dce}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-comment-search/latest.xpi";
-          installation_mode = "force_installed";
-        };
-        "{174b2d58-b983-4501-ab4b-07e71203cb43}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/dark-mode-webextension/latest.xpi";
-          installation_mode = "force_installed";
-        };
-      };
-    };
+    # policies = {
+    #   DisableTelemetry = true;
+    #   DisableFirefoxStudies = true;
+    #   DontCheckDefaultBrowser = true;
+    #   DisablePocket = true;
+    #   SearchBar = "unified";
+    #
+    #   Preferences = {
+    #     # Privacy settings
+    #     "extensions.pocket.enabled" = lock-false;
+    #     "browser.newtabpage.pinned" = lock-empty-string;
+    #     "browser.topsites.contile.enabled" = lock-false;
+    #     "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+    #     "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+    #     "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+    #   };
+    #
+    #   ExtensionSettings = {
+    #     "uBlock0@raymondhill.net" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "extension@tabliss.io" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/file/3940751/tabliss-2.6.0.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "firefox-enpass@enpass.io" = {
+    #       install_url = "https://dl.enpass.io/stable/extensions/firefox/versions/v6.11.7.2/enpass_password_manager-6.11.7.2.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "idcac-pub@guus.ninja" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "user-agent-switcher@ninetailed.ninja" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/uaswitcher/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "{762f9885-5a13-4abd-9c77-433dcd38b8fd}" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/return-youtube-dislikes/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "{b271a84f-6f7c-4784-9b00-3c1b4faa2dce}" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/youtube-comment-search/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #     "{174b2d58-b983-4501-ab4b-07e71203cb43}" = {
+    #       install_url = "https://addons.mozilla.org/firefox/downloads/latest/dark-mode-webextension/latest.xpi";
+    #       installation_mode = "force_installed";
+    #     };
+    #   };
+    # };
   };
 }
