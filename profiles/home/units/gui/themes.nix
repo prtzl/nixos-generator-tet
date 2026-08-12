0cb5f211b3794edef3f1fe050c67b1ed93dcd029
prtzl <matej.blagsic@protonmail.com>
2026-08-12 17:29:22 +0200

    Update flake, change theme due to deprication of materia

--- profiles/home/units/gui/themes.nix
+++ profiles/home/units/gui/themes.nix
@@ -19,8 +19,8 @@
     {
       enable = true;
       theme = {
-        name = "Materia-dark";
-        package = pkgs.materia-theme;
+        name = "Colloid-Dark";
+        package = pkgs.colloid-gtk-theme;
       };
       iconTheme = {
         name = "Papirus-Dark";
